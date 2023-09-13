c167a15414d561ca897a0942cc18bc609a16fce7
amanavi75 <amanavi75@gmail.com>
2023-09-13 11:46:40 +0530

    Added a folder

---
+++ TODOAPP/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema (
+    {
+        title:{
+            type:String,
+            required:true,
+            maxLength:50,
+        },
+        description:{
+            type:String,
+            required:true,
+            maxLength:50,
+        },
+       
+        
+    }
+)
+
+module.exports = mongoose.model("Todo", todoSchema);
